5dc86e19af20fa4b8f626d46c38a8fa575d2ca21
Yuriy Safronnynov <y.safronnikov@gmail.com>
2026-04-29 16:16:56 -0400

    added: tags, negative test as placeholder

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -5,6 +5,7 @@ export class LoginPage extends BasePage {
   private readonly emailInput = this.page.locator('#username');
   private readonly passwordInput = this.page.locator('#password');
   private readonly submitButton = this.page.locator('[data-action-button-primary="true"]');
+  readonly errorMessage = this.page.locator('#error-element-password');
 
   constructor(page: Page) {
     super(page);
